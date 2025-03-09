dd04d41ee910c255f7c968b684fcaf168d1c3739
taylorpitman <123507995+taylorpitman@users.noreply.github.com>
2025-03-09 18:46:41 -0400

    remove unneeded imports

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
-import Home from './pages/index.vue'
-import Admin from './pages/admin.vue'
-import Friends from './pages/friends.vue'
-import Study from './pages/study.vue'
 import { RouterView } from 'vue-router'
 
 import 'bulma/css/bulma.min.css'
